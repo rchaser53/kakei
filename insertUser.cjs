1a3ffa5feb47fcfc877aeb2f6a46b6e7bee9d0e7
rchaser53 <tayoshizawa29@gmail.com>
2025-07-04 08:38:22 +0900

    add util script insertUser.cjs

---
+++ insertUser.cjs
@@ -0,0 +1,45 @@
+const bcrypt = require('bcryptjs');
+const path = require('node:path');
+const sqlite3 = require('sqlite3').verbose();
+
+const DATABASE_PATH = path.join(__dirname, './database.sqlite');
+const db = new sqlite3.Database(DATABASE_PATH);
+
+function insertUser(db, username, password) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      // パスワードをハッシュ化
+      const passwordHash = await bcrypt.hash(password, 10);
+
+      const query = `INSERT INTO users (username, password_hash) VALUES (?, ?)`;
+      db.run(query, [username, passwordHash], function (err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(this.lastID);
+        }
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+const username = process.env.USER_NAME;
+const password = process.env.PASSWORD;
+
+if (username == null || password == null) {
+  console.error(`環境変数 USER_NAME または PASSWORD が設定されていません。`);
+  return;
+}
+
+;(async () => {
+  try {
+    const userId = await insertUser(db, 'testuser', 'password123');
+    console.log('Inserted user with ID:', userId);
+  } catch (error) {
+    console.error('Error inserting user:', error);
+  } finally {
+    db.close();
+  }
+})();
