fa6fcde7852df20a16f7aee7302ab1b6b3d99511
rchaser53 <tayoshizawa29@gmail.com>
2025-07-06 11:43:02 +0900

    fix not to use wrong database.sqlite error

--- packages/backend/src/constants.ts
+++ packages/backend/src/constants.ts
@@ -1,13 +1,13 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import * as appRootDir from 'app-root-dir';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 // ES Moduleでの__dirnameの代替
 const __filename = fileURLToPath(import.meta.url);
 export const __dirname = dirname(__filename);
 
 // データベースパス
-export const DATABASE_PATH = path.join(__dirname, '../database.sqlite');
+export const DATABASE_PATH = path.join(appRootDir.get(), '../../database.sqlite');
 
 // 選択可能な画像ファイル形式
 export const SUPPORTED_IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'png', 'webp'];
